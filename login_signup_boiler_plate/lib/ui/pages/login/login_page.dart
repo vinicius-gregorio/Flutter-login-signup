e79cadfa42a7ef7f4471c42375ca018cacb6a1e1
Vinicius MAC <vincamgreg@hotmail.com>
2021-03-21 15:59:33 -0300

    feat: finish login page UI

--- login_signup_boiler_plate/lib/ui/pages/login/login_page.dart
+++ login_signup_boiler_plate/lib/ui/pages/login/login_page.dart
@@ -10,15 +10,20 @@ class LoginPage extends StatelessWidget {
       return Scaffold(
         body: SafeArea(
           child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 10),
             child: Column(
               children: [
                 Container(
+                  alignment: Alignment.center,
                   padding: EdgeInsets.only(top: 30),
                   height: maxHeight * 0.3,
                   width: maxWidth,
-                  color: Color.fromRGBO(175, 139, 255, 1),
-                  child: Image(
-                    image: AssetImage('lib/ui/assets/logo/logo.png'),
+                  color: Colors.white,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Image(
+                      image: AssetImage('lib/ui/assets/logo/logo.png'),
+                    ),
                   ),
                 ),
                 Padding(padding: EdgeInsets.symmetric(vertical: 10)),
@@ -52,6 +57,46 @@ class LoginPage extends StatelessWidget {
                       ),
                       obscureText: true,
                     ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                          onPressed: () {}, child: Text('Forgot my password')),
+                    ),
+                    Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+                    GestureDetector(
+                      onTap: () {
+                        print('tapped dumbass');
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).primaryColorLight),
+                          width: maxWidth,
+                          child: Text('Entrar',
+                              style: Theme.of(context).textTheme.headline2),
+                        ),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+                    GestureDetector(
+                      onTap: () {
+                        print('tapped dumbass');
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(color: Colors.grey[300]),
+                          width: maxWidth,
+                          child: Text('Cadastrar',
+                              style: Theme.of(context).textTheme.headline2),
+                        ),
+                      ),
+                    )
                   ],
                 ))
               ],
